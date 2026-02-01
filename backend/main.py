eda5f1135bdb22e1f3d22b92b300517c4d72dc71
Buda Dávid <114563545+Budababa@users.noreply.github.com>
2026-02-01 12:07:39 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,20 @@
-from backend.db import engine, Base
-from backend.models.movie_model import Movie
+import os
+import uvicorn
 from fastapi import FastAPI
 from backend.api.movie_routes import router as movie_router
+from backend.db import engine, Base
+from backend.models.movie_model import Movie
 from backend.background.updater import start_background_task
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-@app.get("/")
-def read_root():
-    return {"message": "Movie API is running"}
-
 app.include_router(movie_router)
+
+# Background task
 start_background_task()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
